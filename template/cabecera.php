9a6d3fd1e37abbc75c016dc5f8b7fd2a3535390a
Alain Peralta <104697852+Alain5595@users.noreply.github.com>
2022-05-15 23:02:14 -0500

    template

---
+++ template/cabecera.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+	<script src="js./bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand text-success"><font style="vertical-align: inherit;">Nephro Care</font></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Navegación de palanca">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+        	<li class="nav-item">
+          		<a class="nav-link" href="index.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Inicio
+            		</font></font><span class="visually-hidden"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">(Actual)</font></font></span>
+          		</a>
+        	</li>
+        	<li class="nav-item">
+          		<a class="nav-link" href="index.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Citas</font></font></a>
+        	</li>
+        	<li class="nav-item">
+          		<a class="nav-link" href="index.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Contacto</font></font></a>
+        	</li>
+    
+       		<li class="nav-item dropdown">
+          		<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Servicios</font></font></a>
+				<div class="dropdown-menu">
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Vascular</font></a>
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Nefrologica</font></a>
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Neumologica</font></a>
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Cardiologica</font></a>
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Nutricional</font></a>
+            		<a class="dropdown-item" href="#"><font style="vertical-align: inherit;">Consulta Psicologia</font></a>
+            	</div>
+        	</li>
+      	</ul>
+    <div class="d-flex" id="navbarColor02">
+      	<ul class="navbar-nav me-auto">
+        	<li class="nav-item">
+				<a class="nav-link" href="#"><img src="img/adm.png" width="34" heigth="35"></font></font></a>
+        	</li>	
+		</ul>
+    </div>
+  </div>
+</nav>
+
+    <div class="container">
+    </br>
+	<div class="row">
+		
+
+
+	
